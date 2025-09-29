8ffa98f1af4660f7e5d5e783665102ac12bea613
nafha09 <nh594@exeter.ac.uk>
2025-09-29 11:51:50 +0000

    more files created for working

---
+++ Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    private int id;
+    private int PreferredValue;
+   // hand (maybe nested class ) private List<Integer> Card; 
+   //private leftDeck : Deck 
+   //private rightDeck : Deck 
+   
+
+}
