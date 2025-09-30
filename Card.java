ddc20b9568a5afb119776a9639cac68aaae0075d
AGlomin <arg218@exeter.ac.uk>
2025-09-30 07:48:12 +0000

    finished coding card.java, needs to be tested now

---
+++ Card.java
@@ -0,0 +1,34 @@
+/**
+ * represent a single playing card in the game
+ * 
+ * should be immutable to be thread-safe
+ * cannot be changed
+ */
+public final class Card{
+    private final int value; //immutable
+    /**
+     * constructor
+     * @param value -the integer of card
+     * @throws illegalArgumentException is value is negative
+     */
+     
+    public Card(int value){
+        if(value<0){
+            throw new IllegalArgumentException("Card value must be non-negative");
+        }
+        this.value=value;
+    }
+
+    //Getter
+    public int getValue(){
+        //@returns value of the card
+        return value;
+    }
+
+    //Convert to string for easy printing (not sure might remove later)
+    //@return the string form of the card
+    @Override
+    public String toString(){
+        return String.valueOf(value);
+    }
+}
