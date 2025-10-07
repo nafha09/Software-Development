bf2fbfcdaa11503fb2d4db87126c1a1fe2ac05d8
AGlomin <arg218@exeter.ac.uk>
2025-10-07 06:06:51 +0000

    commented main out for now

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 /////test file for the card class
-public class Main{
-    public static void main(String[] args) {
-        UggRock test1 = new UggRock(UggRockSize.BIGUN, UggRockType.FLOATY);
-        UggRock test2 = new UggRock(UggRockSize.NOT_SO_BIGUN, UggRockType.HOT_HOT_HOT);
+//public class Main{
+    //public static void main(String[] args) {
+        //UggRock test1 = new UggRock(UggRockSize.BIGUN, UggRockType.FLOATY);
+      //  UggRock test2 = new UggRock(UggRockSize.NOT_SO_BIGUN, UggRockType.HOT_HOT_HOT);
 
         //printing
-        System.out.println(test1);
-        System.out.println("Value" + test1.rockValue());
+        //System.out.println(test1);
+        //System.out.println("Value" + test1.rockValue());
 
-    }
-}
+   // }
+//}
